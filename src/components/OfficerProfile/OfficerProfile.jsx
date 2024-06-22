3e0689d0161660b41acc46d40434115ecf939837
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-06-22 21:41:29 +0300

    Create OfficerProfile.jsx

---
+++ src/components/OfficerProfile/OfficerProfile.jsx
@@ -0,0 +1,13 @@
+export default function OfficerProfile({
+  officer: { name, rank, age, spec, status },
+}) {
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Rank: {rank}</p>
+      <p>Age: {age}</p>
+      <p>Spec: {spec}</p>
+      <p>Status: {status}</p>
+    </div>
+  );
+}
