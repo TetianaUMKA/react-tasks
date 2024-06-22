f50c11b0d0d2979ed1a90c9d2918b20a6981a48c
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-06-22 21:41:37 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,30 @@
 import AppBar from "../AppBar/AppBar";
+import OfficerProfile from "../OfficerProfile/OfficerProfile";
 // import "./App.css";
+
+const firstOfficer = {
+  id: 1,
+  name: "Jacob Mercer",
+  rank: "Commander",
+  age: 42,
+  spec: "Fighter Pilot",
+  skills: ["Combat Maneuvers", "Navigation", "Strategic Planning"],
+  status: "Active",
+};
+
+const isModalOpen = true;
+const isUserOnline = false;
 
 export default function App() {
   return (
     <div>
+      {isModalOpen ? <p>Modal is open</p> : <span>Modal is closed</span>}
+      <p>{isUserOnline ? "Active" : "Not active"}</p>
+
+      {isModalOpen && <p>Modal HTML</p>}
+
       <AppBar />
+      <OfficerProfile officer={firstOfficer} />
     </div>
   );
 }
