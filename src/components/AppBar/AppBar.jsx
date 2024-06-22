1d9e130b31023f4a68855d7c1f7c7468198aea06
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-06-22 21:41:35 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,3 +1,11 @@
+import Logo from "../Logo/Logo";
+import UserMenu from "../UserMenu/UserMenu";
+
 export default function AppBar() {
-  return <header>AppBar</header>;
+  return (
+    <header>
+      <Logo />
+      <UserMenu />
+    </header>
+  );
 }
