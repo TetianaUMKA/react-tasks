998c219a534cb9f7ea7a78189fc3a037a3541439
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-06-26 22:18:29 +0300

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,3 +1,12 @@
 export default function Logo() {
-  return <a href="#">Logo</a>;
+  return (
+    <a href="#">
+      <img
+        src="https://img.freepik.com/premium-vector/ens-ethereum-name-service-token-symbol-cryptocurrency-logo-in-circle-coin-icon-isolated-on-white-background_337410-1834.jpg?size=626&ext=jpg&uid=R100662493&ga=GA1.1.380827698.1663862978&semt=ais_user"
+        alt="Logo"
+        width={40}
+      />
+      <p>CoolBrand</p>
+    </a>
+  );
 }
