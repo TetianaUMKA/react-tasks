8013a514c1f20e3389fb60c7a8b6e7da2ddeec1e
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-06-28 22:12:03 +0300

    Create Unit.jsx

---
+++ src/components/Unit/Unit.jsx
@@ -0,0 +1,9 @@
+export default function Unit(props) {
+  return (
+    <div>
+      <p>Unit name: {props.unitName}</p>
+      <p>Number of officers: {props.numberOfficers}</p>
+      <p>Mission: {props.mission}</p>
+    </div>
+  );
+}
