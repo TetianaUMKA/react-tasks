8475e3c2a8c3eeea35f2885843368306cfef064a
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-07-05 21:21:09 +0300

    Update OfficerProfile.jsx

--- src/components/OfficerProfile/OfficerProfile.jsx
+++ src/components/OfficerProfile/OfficerProfile.jsx
@@ -1,5 +1,5 @@
 export default function OfficerProfile({
-  officer: { name, rank, age, spec, status },
+  officer: { name, rank, age, spec, status, skills },
 }) {
   return (
     <div>
@@ -7,6 +7,11 @@ export default function OfficerProfile({
       <p>Rank: {rank}</p>
       <p>Age: {age}</p>
       <p>Spec: {spec}</p>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
       <p>
         Status:{" "}
         {status === "Active" ? "available for hire" : "not available for hire"}
