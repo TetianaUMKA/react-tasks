8e4be25b4b2648770509d6073bbeccfc7e1e0a7f
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-07-07 23:37:52 +0300

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,17 @@
+import logoURL from "./pexels-artyusufpatel-9949421.jpg";
+
 export default function Logo() {
   return (
-    <a href="#">
-      <img
-        src="https://img.freepik.com/premium-vector/ens-ethereum-name-service-token-symbol-cryptocurrency-logo-in-circle-coin-icon-isolated-on-white-background_337410-1834.jpg?size=626&ext=jpg&uid=R100662493&ga=GA1.1.380827698.1663862978&semt=ais_user"
-        alt="Company logo"
-        width={40}
-      />
-      <span>CoolBrand</span>
-    </a>
+    <div>
+      <a href="#">
+        <img
+          src="https://img.freepik.com/premium-vector/ens-ethereum-name-service-token-symbol-cryptocurrency-logo-in-circle-coin-icon-isolated-on-white-background_337410-1834.jpg?size=626&ext=jpg&uid=R100662493&ga=GA1.1.380827698.1663862978&semt=ais_user"
+          alt="Company logo"
+          width={40}
+        />
+        <span>CoolBrand</span>
+      </a>
+      <img src={logoURL} alt="Logo test" width={60} />
+    </div>
   );
 }
