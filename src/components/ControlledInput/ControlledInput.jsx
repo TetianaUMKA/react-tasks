25ef2d76fe77f534f93dbf31495d1a3f7b835ee9
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-21 19:09:21 +0300

    Create ControlledInput.jsx

---
+++ src/components/ControlledInput/ControlledInput.jsx
@@ -0,0 +1,12 @@
+import css from "./ControlledInput.module.css";
+
+export default function ControlledInput({ value, onUpdate }) {
+  const handleChange = (event) => {
+    onUpdate(event.target.value);
+  };
+  return (
+    <div className={css.container}>
+      <input type="text" value={value} onChange={handleChange} />
+    </div>
+  );
+}
