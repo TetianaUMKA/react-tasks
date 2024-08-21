4e8afd39c31086c46734a8540888cc396023aec1
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-21 19:09:25 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,12 @@
 import AppBar from "../AppBar/AppBar";
+import officers from "../../officers.json";
+
 import OfficerProfile from "../OfficerProfile/OfficerProfile";
 import OfficerList from "../OfficerList/OfficerList";
 import Unit from "../Unit/Unit";
-import officers from "../../officers.json";
+import Form from "../UserForm/UserForm";
+import { useState } from "react";
+import ControlledInput from "../ControlledInput/ControlledInput";
 
 const firstOfficer = {
   id: 1,
@@ -27,23 +31,35 @@ const isModalOpen = true;
 const isUserOnline = true;
 
 export default function App() {
+  const addNewUser = (newUser) => {
+    console.log(newUser);
+  };
+
+  const [text, setText] = useState("");
+
+  const handleTextChange = (newText) => {
+    setText(newText);
+  };
+
   return (
     <div>
       {isModalOpen ? <p>Modal is open</p> : <span>Modal is closed</span>}
       <p>{isUserOnline ? "Active" : "Not active"}</p>
-
       <p>
         Is anybody here? Answer: {(isUserOnline && "Officer") || "Nobody"} is
         here.
       </p>
-
       {isModalOpen && <p>Modal HTML</p>}
-
       <AppBar />
       <Unit unitName="the falcon" numberOfficers={9} mission="defense" />
       <OfficerList items={officers} />
       <OfficerProfile officer={firstOfficer} />
       <OfficerProfile officer={secondOfficer} />
+      <Form onAddNewUser={addNewUser} />
+
+      {/* controlled element*/}
+      <ControlledInput value={text} onUpdate={handleTextChange} />
+      <p>{text}</p>
     </div>
   );
 }
