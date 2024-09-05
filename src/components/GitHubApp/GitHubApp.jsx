bd47867d9f104570e2b2261c820ae0bee4ef4641
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-09-05 22:50:30 +0300

    Create https request

--- src/components/GitHubApp/GitHubApp.jsx
+++ src/components/GitHubApp/GitHubApp.jsx
@@ -1,20 +1,37 @@
 import { Field, Form, Formik } from "formik";
 // import * as Yup from "yup";
-import axios from "axios";
 import { useState } from "react";
 
+import { fetchUser } from "../../services/github-api";
+
 export default function GitHubApp() {
+  // About the initial value of useState 💡
+  // boolean 👉 true false
+  // 123 👉 0
+  // [1,2,3] 👉 []
+  // {} 👉 null
+
   const [user, setUser] = useState(null);
 
-  const handleSubmit = async (values, actions) => {
-    console.log(values);
+  // way 1
+  // const handleSubmit = async (values, actions) => {
+  //   console.log(values);
 
-    const response = await axios.get(
-      `https://api.github.com/users/${values.username}`
-    );
-    console.log(response);
-    console.log(response.data);
+  //   const response = await axios.get(
+  //     `https://api.github.com/users/${values.username}`
+  //   );
+  //   console.log(response);
+  //   console.log(response.data);
+  //   setUser(response.data);
+  // };
+
+  //way 2 (output the function with axios into another file with name github-api.js)
+  const handleSubmit = async (values, actions) => {
+    const fetchedUser = await fetchUser(values.username);
+    setUser(fetchedUser);
+    actions.resetForm();
   };
+
   return (
     <div>
       <h1>HTTP requests in React</h1>
@@ -24,6 +41,25 @@ export default function GitHubApp() {
           <button type="submit">Search</button>
         </Form>
       </Formik>
+
+      {user && (
+        <div>
+          <img src={user.avatar_url} alt={user.name} />
+          <p>{user.name}</p>
+          <a href={user.html_url} target="_blank">
+            GitHub Page
+          </a>
+          <p>Followers: {user.followers}</p>
+          <p>{user.bio}</p>
+        </div>
+      )}
     </div>
   );
 }
+
+// from response.data:
+// name
+// avatar_url
+// html_url
+// followers
+// bio
