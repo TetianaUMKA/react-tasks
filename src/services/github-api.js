5087a54b2090c0247b4585a9468510d64e5203b8
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-09-06 19:13:40 +0300

    Update github-api.js

--- src/services/github-api.js
+++ src/services/github-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const fetchUser = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
+  const response = await axios.get(`https://api.github.com/users1/${username}`);
   return response.data;
 };
